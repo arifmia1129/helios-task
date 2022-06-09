b8d068d4586a33c506c1fbc25c3dad1940d30223
arifvtti <arif.vtti@gmail.com>
2022-06-09 09:23:31 +0600

    added success message in ui when user info saved successfully

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -3,14 +3,18 @@ import addDb from '../utilities/fakeDB';
 import "./UserForm.css";
 const UserForm = () => {
     const [error, setError] = useState("");
+    const [successMsg, setSuccessMsg] = useState("");
     const handleUserInfo = e => {
         e.preventDefault();
         setError("");
+        setSuccessMsg("");
         const name = e.target.name.value;
         const phone = e.target.phone.value;
         if (/^\+?(8801)[3456789][0-9]{8}$/.test(phone)) {
             const user = { name, phone };
             addDb(user);
+            e.target.reset();
+            setSuccessMsg("Your information successfully saved!")
         }
         else {
             setError("Invalid phone number. Please try with country code.")
@@ -25,6 +29,7 @@ const UserForm = () => {
                 <label for="lname">Phone number</label>
                 <input type="text" id="phone" name="phone" placeholder="Your phone number.." />
                 <p id='error-msg'><small>{error && error}</small></p>
+                <p id='success-msg'><small>{successMsg && successMsg}</small></p>
                 <input type="submit" value="Submit" />
             </form>
         </div>
