6cb27988a87a75f40514afc710e8b0baec10a92c
arifvtti <arif.vtti@gmail.com>
2022-06-09 11:57:48 +0600

    use home btn in user detail page

--- src/components/UserDetail/UserDetail.js
+++ src/components/UserDetail/UserDetail.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import useUser from '../../hooks/useUser';
 import "./UserDetail.css";
 const UserDetail = () => {
+    const navigate = useNavigate();
     const [users] = useUser();
     const { phone } = useParams();
     const user = users?.find(u => u.phone === phone);
     return (
         <div>
+            <p onClick={() => navigate("/")} style={{ marginLeft: "20px", cursor: "pointer" }}>Home</p>
             <h3 id='title'>User Details</h3>
             <div id='user'>
                 <p><span className='point'>User name:</span> {user?.name}</p>
