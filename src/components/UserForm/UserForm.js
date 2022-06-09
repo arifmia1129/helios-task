0540b669f859ff98b444b8a0137497c7a7fd4d56
arifvtti <arif.vtti@gmail.com>
2022-06-09 08:48:26 +0600

    check phone number validation and display error

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./UserForm.css";
 const UserForm = () => {
+    const [error, setError] = useState("");
+    const handleUserInfo = e => {
+        e.preventDefault();
+        setError("");
+        const name = e.target.name.value;
+        const phone = e.target.phone.value;
+        if (/^\+?(8801)[3456789][0-9]{8}$/.test(phone)) {
+            console.log(name, phone);
+        }
+        else {
+            setError("Invalid phone number. Please try with country code.")
+        }
+    }
     return (
         <div>
-            <form>
+            <form onSubmit={handleUserInfo}>
                 <h3 id='form-title'>User Info Collect Form</h3>
                 <label for="fname">Name</label>
                 <input type="text" id="name" name="name" placeholder="Your name.." />
 
                 <label for="lname">Phone number</label>
                 <input type="text" id="phone" name="phone" placeholder="Your phone number.." />
+                <p id='error-msg'><small>{error && error}</small></p>
                 <input type="submit" value="Submit" />
             </form>
         </div>
